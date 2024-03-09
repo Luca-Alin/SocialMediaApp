bc52dc76ab973eb7d5fa6267acc7d52c873177fa
luca-alin <alinluca20021@gmail.com>
2024-03-09 18:00:23 +0200

    Deleted some unecessary files

--- ui/src/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseApiUrl: "https://social-media-app-java.azurewebsites.net/api/v1",
-  baseWsUrl: "ws://social-media-app-java.azurewebsites.net"
-};
--- ui/src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  baseApiUrl: "http://localhost:8080/api/v1",
-  baseWsUrl: "ws://localhost:8080"
-};
