58d1114abccd5742aaaa45aa5ee5176eecb9654f
luca-alin <alinluca20021@gmail.com>
2024-03-09 23:38:42 +0200

    added a constructor overload

--- src/main/java/springboottemplate/data_services/exception/UserNotFoundException.java
+++ src/main/java/springboottemplate/data_services/exception/UserNotFoundException.java
@@ -2,6 +2,10 @@ package springboottemplate.data_services.exception;
 
 public class UserNotFoundException extends Exception {
 
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
     public UserNotFoundException(String email) {
         super("User with email %s not found".formatted(email));
     }
