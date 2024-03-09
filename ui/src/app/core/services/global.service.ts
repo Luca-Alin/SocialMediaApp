20245e0ee5cf05020d465deef1d2af58e756751a
luca-alin <alinluca20021@gmail.com>
2024-03-09 18:28:40 +0200

    Revert "Removed the reference to the environment file"
    This reverts commit 15da0a8c76671f3ba80c06619442e1405d798488.

--- ui/src/app/core/services/global.service.ts
+++ ui/src/app/core/services/global.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {UserDTO} from "../http/user-service/model/UserDTO";
+import {environment} from "../../../environment/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
   baseUrl: string = "http://localhost:8080/api/v1";
+  // baseUrl: string = environment.baseApiUrl;
   user: UserDTO | null = null;
   constructor() {
     console.log(this.baseUrl);
