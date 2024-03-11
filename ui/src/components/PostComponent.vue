077a42129f1f0caa3e570d0955d904a4680aeca9
luca-alin <alinluca20021@gmail.com>
2024-03-12 00:25:49 +0200

    Added boilerplate more for the post component

---
+++ ui/src/components/PostComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type {PostDTO} from "../services/post-service/model/PostDTO";
+import {onMounted, type Ref, ref} from "vue";
+
+const props = defineProps({
+  propPost: Object
+});
+
+const post : Ref<PostDTO | null> = ref(null);
+
+onMounted(() => {
+  post.value = props.propPost as PostDTO;
+})
+</script>
+
+<template>
+<div class="container">
+  <div>
+    {{ post?.content }}
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
