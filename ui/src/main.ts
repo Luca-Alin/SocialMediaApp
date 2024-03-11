f5e84ac62099b8a93c6b1209a10cc3df0535219e
luca-alin <alinluca20021@gmail.com>
2024-03-11 22:20:21 +0200

    merged the axios interceptor file with main.ts

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -6,6 +6,39 @@ import {createPinia} from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import axios from "axios";
+
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+export const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use(
+    (config) => {
+        if (config.url?.indexOf("/public"))
+            return config;
+
+        const accessToken = localStorage.getItem("accessToken");
+        if (!accessToken) {
+            router.push("").finally();
+        }
+
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
 const app = createApp(App);
 
 app.use(createPinia());
