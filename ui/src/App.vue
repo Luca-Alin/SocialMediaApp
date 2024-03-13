6a39a50c1c3b4e3cd52c511f3f0122c68d3129d4
luca-alin <alinluca20021@gmail.com>
2024-03-13 21:28:44 +0200

    Changed the layout

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+import ChatComponent from "./components/ChatComponent.vue";
+import NavbarComponent from "./components/NavbarComponent.vue";
+import {useUserInfoStore} from "./stores/UserInfoStore";
+import {storeToRefs} from "pinia";
+
+const userStore = useUserInfoStore();
+const {user} = storeToRefs(userStore);
 </script>
 
 <template>
+  <div class="container d-flex flex-column">
 
-  <RouterView/>
+    <div>
+      <NavbarComponent/>
+    </div>
+
+    <div>
+      <div class="d-flex flex-row">
+        <div class="container">
+          <RouterView/>
+        </div>
+
+        <div v-if="user" class="container" style="max-height: 500px">
+          <ChatComponent style="max-height: 500px; max-width: 300px; position: absolute; right: 0; bottom: 0"/>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <style scoped>
