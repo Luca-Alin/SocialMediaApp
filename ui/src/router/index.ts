4a5948d5671f3bed0d4dd1eaa3d26136df0f7af9
luca-alin <alinluca20021@gmail.com>
2024-03-13 21:28:23 +0200

    Removed the home page from the router

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,5 +1,4 @@
 import {createRouter, createWebHistory} from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
