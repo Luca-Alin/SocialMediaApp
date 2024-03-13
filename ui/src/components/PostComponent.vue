d12782b20cac9cea5a543a31e64a9fbf86951b9e
luca-alin <alinluca20021@gmail.com>
2024-03-13 21:27:58 +0200

    Improved the post component

--- ui/src/components/PostComponent.vue
+++ ui/src/components/PostComponent.vue
@@ -6,19 +6,55 @@ const props = defineProps({
   propPost: Object
 });
 
-const post : Ref<PostDTO | null> = ref(null);
+const post: Ref<PostDTO | null> = ref(null);
 
 onMounted(() => {
   post.value = props.propPost as PostDTO;
-})
+});
+
+const slide: any = ref(0);
+const sliding: any = ref(null);
+
+function onSlideStart(slide: any) {
+  sliding.value = true;
+}
+
+function onSlideEnd(slide: any) {
+  sliding.value = false;
+}
 </script>
 
 <template>
-<div class="container">
-  <div>
-    {{ post?.content }}
+  <div class="container border border-1">
+
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+
+        <div v-for="image in post?.images">
+          <div class="carousel-item active">
+            <img class="d-block w-100" :src="`data:image/png;base64,${image}`" alt="First slide">
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div>
+      <router-link :to="{
+      name: `User`,
+      params: {
+        id: `${post?.user.uuid}`
+      }
+    }"
+      >{{ post?.user.firstName }} {{ post?.user.lastName }}
+      </router-link>
+    </div>
+    <div>
+      {{ post?.content }}
+    </div>
+
   </div>
-</div>
+
 </template>
 
 <style scoped>
---
+++ ui/src/components/PostsComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<p>Here are the posts</p>
+</template>
+
+<style scoped>
+
+</style>
