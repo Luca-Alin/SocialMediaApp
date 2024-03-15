c64d107b9eae804db2595188f7c37eb4f8f10085
luca-alin <alinluca20021@gmail.com>
2024-03-15 13:53:32 +0200

    Made the layout more dynamic

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,9 +3,12 @@ import ChatComponent from "./components/ChatComponent.vue";
 import NavbarComponent from "./components/NavbarComponent.vue";
 import {useUserInfoStore} from "./stores/UserInfoStore";
 import {storeToRefs} from "pinia";
+import {type Ref, ref} from "vue";
 
 const userStore = useUserInfoStore();
 const {user} = storeToRefs(userStore);
+
+const chatWindowIsOpen: Ref<boolean> = ref(false);
 </script>
 
 <template>
@@ -15,19 +18,39 @@ const {user} = storeToRefs(userStore);
       <NavbarComponent/>
     </header>
 
-    <main class="overflow-y-scroll">
+    <main class="container-fluid d-flex flex-row">
 
-      <div class="d-flex flex-row">
-        <div class="container d-flex justify-content-center">
-          <div>
-            <RouterView/>
-          </div>
-        </div>
-
-        <div v-if="user" class="container" style="max-height: 500px">
-          <ChatComponent style="max-height: 500px; max-width: 300px; position: absolute; right: 3em; bottom: 0"/>
+      <!-- Content - posts, settings, user profiles etc. -->
+      <div :class="(chatWindowIsOpen) ?
+                    'd-none d-sm-none d-md-flex'
+                    : 'd-flex'">
+        <div style="max-width: 768px">
+          <RouterView/>
         </div>
       </div>
+
+
+      <!-- Chat Window Button  -->
+      <div v-if="!chatWindowIsOpen" class="open-chat-button d-md-none" style="z-index: 9999;">
+        <button
+            type="button"
+            class="btn btn-primary rounded-5 pt-2 pb-2"
+            @click="chatWindowIsOpen = !chatWindowIsOpen"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16"><path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"></path></svg>
+        </button>
+      </div>
+
+      <!-- Actual Chat Window -->
+      <div v-if="user"
+           :class="(chatWindowIsOpen) ?
+             'd-flex d-sm-flex w-100 h-100'
+             : 'd-none d-sm-none d-md-flex w-100 h-100'"
+      >
+        <button v-if="chatWindowIsOpen" @click="chatWindowIsOpen = false">Close Chat</button>
+        <ChatComponent/>
+      </div>
+
     </main>
 
 
@@ -35,6 +58,12 @@ const {user} = storeToRefs(userStore);
 </template>
 
 <style scoped>
+.open-chat-button {
+  position: absolute;
+  right: 1.5em;
+  bottom: 1.5em;
+}
+
 main::-webkit-scrollbar {
   width: 0.4em;
 }
