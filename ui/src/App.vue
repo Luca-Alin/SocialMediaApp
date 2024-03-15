2a05a9d8c9bda3b0821911041cfddb0a1b43dc6e
luca-alin <alinluca20021@gmail.com>
2024-03-15 16:04:18 +0200

    Made the chat window more responsive

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,17 +8,22 @@ import {type Ref, ref} from "vue";
 const userStore = useUserInfoStore();
 const {user} = storeToRefs(userStore);
 
-const chatWindowIsOpen: Ref<boolean> = ref(false);
+const chatWindowIsOpen: Ref<boolean> = ref(true);
+
+const childMsg = ref('No child msg yet')
 </script>
 
 <template>
+  <div>
+    {{ chatWindowIsOpen }}
+  </div>
   <div id="main" class="d-flex flex-column vh-100 min-vh-100">
 
     <header>
       <NavbarComponent/>
     </header>
 
-    <main class="container-fluid d-flex flex-row">
+    <main class="d-flex flex-row flex-fill justify-content-center">
 
       <!-- Content - posts, settings, user profiles etc. -->
       <div :class="(chatWindowIsOpen) ?
@@ -37,18 +42,31 @@ const chatWindowIsOpen: Ref<boolean> = ref(false);
             class="btn btn-primary rounded-5 pt-2 pb-2"
             @click="chatWindowIsOpen = !chatWindowIsOpen"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16"><path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat"
+               viewBox="0 0 16 16">
+            <path
+                d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"></path>
+          </svg>
         </button>
       </div>
 
-      <!-- Actual Chat Window -->
+      <!-- Chat Window -->
       <div v-if="user"
            :class="(chatWindowIsOpen) ?
-             'd-flex d-sm-flex w-100 h-100'
-             : 'd-none d-sm-none d-md-flex w-100 h-100'"
+             'd-flex d-sm-flex flex-fill'
+             : 'd-none d-sm-none d-md-flex'"
       >
-        <button v-if="chatWindowIsOpen" @click="chatWindowIsOpen = false">Close Chat</button>
-        <ChatComponent/>
+        <div
+            class="overflow-y-scroll d-flex flex-column flex-grow-1"
+        >
+          <ChatComponent
+              @maximize-request="() => chatWindowIsOpen = !chatWindowIsOpen"
+              :chat-is-maximised="chatWindowIsOpen"
+              class="flex-fill overflow-y-scroll"
+              style="max-height: 75vh"
+          />
+        </div>
+
       </div>
 
     </main>
@@ -59,7 +77,7 @@ const chatWindowIsOpen: Ref<boolean> = ref(false);
 
 <style scoped>
 .open-chat-button {
-  position: absolute;
+  position: fixed;
   right: 1.5em;
   bottom: 1.5em;
 }
--- ui/src/components/ChatComponent.vue
+++ ui/src/components/ChatComponent.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
-import {createPinia, storeToRefs} from "pinia";
-import {createApp, onMounted, type Ref, ref, watch} from "vue";
-import App from "../App.vue";
+import {storeToRefs} from "pinia";
+import {onMounted, type Ref, ref, watch} from "vue";
 import {useUserInfoStore} from "../stores/UserInfoStore";
 import router from "../router";
 import chatService from "../services/chat-service/ChatService";
 import messageService from "../services/message-service/MessageService";
 import {useMessagesStore} from "../stores/MessagesStore";
+import type {UserDTO} from "../services/user-service/model/UserDTO";
 
-const pinia = createPinia();
-const app = createApp(App);
-app.use(pinia);
 const userInfo = useUserInfoStore();
 const {user, accessToken, refreshToken} = storeToRefs(userInfo);
 
-const chatIsMaximised = ref(false);
+
 
 const socketIsConnected = ref(chatService.webSocketIsConnected);
 
 const messageStore = useMessagesStore();
 const {conversations} = storeToRefs(messageStore);
+
+const selectedUser: Ref<UserDTO | null> = ref(null);
+
+const props = defineProps({
+  chatIsMaximised: Boolean
+})
+const emit = defineEmits(["maximize-request"]);
 
 // TODO - reduce the boilerplatness of this code
 watch(accessToken, () => {
@@ -27,18 +31,17 @@ watch(accessToken, () => {
     router.push("/login");
 });
 onMounted(() => {
-
   try {
-    chatService.connect();
     messageService.findAllConversations()
         .then(res => {
+          console.log(res.data);
           messageStore.conversations = res.data;
         })
         .catch(err => {
-          console.log(err)
-        })
-  }
-  catch (err) {
+          console.log(err);
+        });
+    chatService.connect();
+  } catch (err) {
     console.log(err);
   }
 
@@ -47,23 +50,55 @@ onMounted(() => {
     return;
   }
 });
-
 </script>
 
 <template>
-  <div class="border border-primary border-2 overflow-y-scroll">
+  <div class="border border-primary">
     <div class="d-flex justify-content-center align-content-center">
       <button
-          class="btn btn-primary w-100 h-100 rounded-0"
-          v-if="socketIsConnected" @click="chatIsMaximised = !chatIsMaximised"
-      >Chat is live</button>
+          class="btn btn-outline-primary w-100 h-100 rounded-0"
+          v-if="socketIsConnected"
+          @click="/*chatIsMaximised = !chatIsMaximised;*/
+          emit('maximize-request', 'maximize');
+          chatIsMaximised = !chatIsMaximised
+          "
+      >Chat is live
+      </button>
       <p v-else>Chat is offline</p>
     </div>
 
-    <div  class="container" v-if="chatIsMaximised">
-      <div v-for="conversation in conversations">
-        <div>{{ conversation.friend.firstName }} {{ conversation.friend.lastName }}</div>
+    <div v-if="chatIsMaximised">
+
+      <!-- Chat Panel -->
+      <div v-if="selectedUser">
+        <div class="d-flex">
+          <header class="d-flex bg-primary w-100">
+            <button type="button" class="btn btn-primary" @click="selectedUser = null">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                   class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"></path>
+              </svg>
+            </button>
+            <button class="btn btn-primary w-100">
+              {{ selectedUser?.firstName }} {{ selectedUser?.lastName }}
+            </button>
+          </header>
+        </div>
+
+        <div class="d-flex">
+          <input type="text">
+          <button @click="">Send</button>
+        </div>
       </div>
+
+      <!-- Conversations Panel -->
+      <div v-else v-for="conversation in conversations">
+        <button class="btn" @click="selectedUser = conversation.friend">
+          {{ conversation.friend.firstName }} {{ conversation.friend.lastName }}
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
