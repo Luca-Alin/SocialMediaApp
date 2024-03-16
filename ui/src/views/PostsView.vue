a89f838734bbf0200a68033229451f52df666ff2
luca-alin <alinluca20021@gmail.com>
2024-03-16 19:52:58 +0200

    Added functionality for comments

--- ui/src/views/PostsView.vue
+++ ui/src/views/PostsView.vue
@@ -1,45 +1,62 @@
 <script setup lang="ts">
 import postService from "../services/post-service/PostService";
-import {createApp, onMounted, ref, watch} from "vue";
+import {onMounted, watch} from "vue";
 import PostComponent from "../components/PostComponent.vue";
 import router from "../router";
-import {createPinia, storeToRefs} from "pinia";
+import FakePost from "../components/FakePost.vue";
+
+import {storeToRefs} from "pinia";
 import {useUserInfoStore} from "../stores/UserInfoStore";
+import {usePostsStore} from "../stores/PostsStore";
 
 const userInfo = useUserInfoStore();
 const {user, accessToken, refreshToken} = storeToRefs(userInfo);
 
-const posts = ref([]);
+const postsStore = usePostsStore();
+const {posts} = storeToRefs(postsStore);
+
 
 // TODO - reduce the boilerplatness of this code
-onMounted(() => {
-  if (accessToken.value == null) {
-    router.push("login");
-    return;
-  }
-
-  watch(accessToken, (oldVal, newVal) => {
-    if (accessToken.value == null)
-      router.push("login");
-  })
-
+function loadPosts(page : number, count: number) {
+  postsStore.addFakePosts(5);
   postService.findAllByUserAndFriends()
       .then((res) => {
-        posts.value = res.data;
+        postsStore.removeAllFakePosts();
+        postsStore.addPosts(res.data);
       })
       .catch((err) => {
         alert(err);
       })
       .finally(() => {
       });
+}
+
+onMounted(() => {
+
+  if (accessToken.value == null) {
+    router.push("login");
+    return;
+  }
+
+  watch(accessToken, _ => {
+    if (accessToken.value == null)
+      router.push("login");
+  });
+
+  loadPosts(0, 0);
 });
 </script>
 
 <template>
   <h1>Posts</h1>
 
-  <div v-for="post in posts" class="d-flex flex-row">
-    <PostComponent :propPost="post"/>
+  <div v-for="post in posts.values()" class="d-flex flex-row">
+    <div v-if="post === null">
+      <FakePost/>
+    </div>
+    <div v-else>
+      <PostComponent :post-id="post.uuid"/>
+    </div>
   </div>
 </template>
 
