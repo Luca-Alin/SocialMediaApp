3931dbf79af92b0d6a4f1f7d6342a782ea65f8c1
luca-alin <alinluca20021@gmail.com>
2024-03-16 12:25:14 +0200

    Added a fake post loading screen

---
+++ ui/src/components/FakePost.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container border border-1 p-1">
+    <div>
+      <p> John Smith</p>
+    </div>
+
+    <!-- Post Images Carousel -->
+    <div class="carousel carousel-dark slide">
+      <div class="carousel-indicators">
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
+               xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide"
+               preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#f5f5f5" data-darkreader-inline-fill=""
+                  style="--darkreader-inline-fill: #1e2021;"></rect>
+            <text x="50%" y="50%" fill="#aaa" dy=".3em" data-darkreader-inline-fill=""
+                  style="--darkreader-inline-fill: #b2aca2;">Loading...
+            </text>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
