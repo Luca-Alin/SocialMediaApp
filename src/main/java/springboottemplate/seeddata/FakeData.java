60f3b4da108a616def458d8d9b00de51f7384d6b
Alin Luca <alinluca20021@gmail.com>
2024-04-29 02:49:13 +0300

    97% faster seed data generation

--- src/main/java/springboottemplate/seeddata/FakeData.java
+++ src/main/java/springboottemplate/seeddata/FakeData.java
@@ -1,12 +1,10 @@
 package springboottemplate.seeddata;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import springboottemplate.data_services.comment.model.Comment;
 import springboottemplate.data_services.comment.repository.CommentRepository;
 import springboottemplate.data_services.friendship.model.Friendship;
@@ -23,12 +21,7 @@ import springboottemplate.data_services.user.model.UserProfile;
 import springboottemplate.data_services.user.repository.UserRepository;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
 import java.util.*;
 
 
@@ -39,86 +32,70 @@ import java.util.*;
 public class FakeData {
 
 
-    private int textIndex = 1;
-    private int imageIndex = 1;
     private final PostRepository postRepository;
     private final FriendshipRepository friendshipRepository;
     private final Random random = new Random();
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
     private final MessageRepository messageRepository;
-    private final ObjectMapper mapper = new ObjectMapper();
+    public List<byte[]> images = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream("/images.txt"))))
+            .lines()
+            .map(line -> Base64.getDecoder().decode(line))
+            .toList();
+    public List<String> quotes = new BufferedReader(new InputStreamReader(Objects.requireNonNull(this.getClass().getResourceAsStream("/quotes.txt"))))
+            .lines()
+            .toList();
 
-//    @Bean
+
+    @Bean
     public CommandLineRunner commandLineRunner() {
         return _ -> {
-            long timer = System.currentTimeMillis();
-            generateUsers();
-            System.out.printf("Time to generate users: %s%n", (System.currentTimeMillis() - timer) / 1000.0);
+            double time = System.currentTimeMillis();
 
-            timer = System.currentTimeMillis();
+            generateUsers("John Smith");
+            generateUsers("James Doe");
+            generateUsers("Robert Doe");
+            generateUsers("Maria Rodriguez");
+
             generateFriendships();
-            System.out.printf("Time to generate friendships: %s%n", (System.currentTimeMillis() - timer) / 1000.0);
 
-            timer = System.currentTimeMillis();
             generatePosts();
-            System.out.printf("Time to generate posts: %s%n", (System.currentTimeMillis() - timer) / 1000.0);
 
-            timer = System.currentTimeMillis();
             generateComments();
-            System.out.printf("Time to generate comments: %s%n", (System.currentTimeMillis() - timer) / 1000.0);
 
-            timer = System.currentTimeMillis();
             generateMessages();
-            System.out.printf("Time to generate messages: %s%n", (System.currentTimeMillis() - timer) / 1000.0);
 
-            System.out.println("Done!");
+            System.out.printf("Database data generated in: %.2fs\n", (System.currentTimeMillis() - time) / 1000d);
         };
     }
 
-
-    public void generateUsers() {
-        String hashedPassword = BCrypt.hashpw("password", BCrypt.gensalt());
-
-        List<String> usersList = new ArrayList<>(List.of(
-                "John Smith",
-                "James Doe",
-                "Robert Doe",
-                "Maria Rodriguez"
-        ));
+    public void generateUsers(String fullName) {
+        // this is a hash for the word "password"
+        String hashedPassword = "$2a$10$SOAyQMmhTFRbf9drP3Myy.NDNNnaJQkMUhxauq2LucaCSBjwIciAK";
 
 
-        List<User> users = new ArrayList<>();
-        usersList.stream()
-                .parallel()
-                .forEach(fullName -> {
+        String firstName = fullName.split(" ")[0];
+        String lastName = fullName.split(" ")[1];
+        String email = "%s_%s@social-media.app".formatted(firstName.toLowerCase(), lastName.toLowerCase());
+        String bio = "Hello, I am %s %s.".formatted(firstName, lastName);
 
-                    String firstName = fullName.split(" ")[0];
-                    String lastName = fullName.split(" ")[1];
-                    String email = "%s_%s@social-media.app".formatted(firstName.toLowerCase(), lastName.toLowerCase());
-                    byte[] profileImage = randomImage();
-                    String bio = "Hello, I am %s %s.".formatted(firstName, lastName);
+        User user = User
+                .builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .userProfile(
+                        UserProfile
+                                .builder()
+                                .profileImage(randomImage())
+                                .bio(bio)
+                                .build()
+                )
+                .password(hashedPassword)
+                .role(Role.USER)
+                .build();
 
-                    User user = User
-                            .builder()
-                            .firstName(firstName)
-                            .lastName(lastName)
-                            .email(email)
-                            .userProfile(
-                                    UserProfile
-                                            .builder()
-                                            .profileImage(profileImage)
-                                            .bio(bio)
-                                            .build()
-                            )
-                            .password(hashedPassword)
-                            .role(Role.USER)
-                            .build();
-
-                    users.add(user);
-                });
-
-        userRepository.saveAll(users);
+        userRepository.save(user);
     }
 
     public void generateFriendships() {
@@ -143,8 +120,6 @@ public class FakeData {
     }
 
     private void generatePosts() {
-        List<Post> posts = new ArrayList<>();
-
         List<User> users = userRepository.findAll();
 
         List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
@@ -210,7 +185,7 @@ public class FakeData {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 20; i++) list.add(i);
 
-        list.stream().parallel().forEach((integer) -> {
+        list.stream().parallel().forEach(_ -> {
             User sender = users.get(random.nextInt(users.size()));
             User receiver = users.get(random.nextInt(users.size()));
             if (sender.equals(receiver)) return;
@@ -233,62 +208,11 @@ public class FakeData {
     }
 
     public byte[] randomImage() {
-        byte[] imageBytes;
-
-        try {
-            int width = random.nextInt(300, 600);
-            int height = random.nextInt(300, 600);
-            String url = "https://picsum.photos/id/%d/%d/%d".formatted(imageIndex++, width, height);
-
-            URL requestUrl = new URI(url).toURL();
-
-            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
-            connection.setRequestMethod("GET");
-
-            InputStream inputStream = connection.getInputStream();
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            imageBytes = outputStream.toByteArray();
-            connection.disconnect();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return imageBytes;
+        return images.get(random.nextInt(images.size()));
     }
 
     public String randomText() {
-        String text;
-        try {
-            String url = "https://dummyjson.com/quotes/%d".formatted(textIndex++); // Replace this URL with your JSON API URL
-            URL requestUrl = new URI(url).toURL();
-            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-
-            FakeQuote quote = mapper.readValue(response.toString(), FakeQuote.class);
-            text = quote.getQuote();
-
-            connection.disconnect();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
-        return text;
+        return quotes.get(random.nextInt(quotes.size()));
     }
 
 }
