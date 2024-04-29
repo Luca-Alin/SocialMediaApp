d86b12cb5140166dde081ceae612d3231a861f98
Alin Luca <alinluca20021@gmail.com>
2024-04-29 08:41:57 +0300

    Added a 2-Map system for storing connected users

--- src/main/java/springboottemplate/data_services/chat/ChatWebSocketHandler.java
+++ src/main/java/springboottemplate/data_services/chat/ChatWebSocketHandler.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -12,23 +14,25 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import springboottemplate.data_services.chat.service.ChatService;
-import springboottemplate.data_services.message.model.Message;
 import springboottemplate.data_services.message.model.MessageDTO;
 import springboottemplate.data_services.message.model.MessageRequest;
 import springboottemplate.data_services.message.service.MessageService;
 import springboottemplate.data_services.user.model.User;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 
 @Component
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     private final MessageService messageService;
-    private final Map<String, WebSocketSession> map = new HashMap<>();
+
+    private final Map<String, WebSocketSession> userIds = new ConcurrentHashMap<>();
+    private final Map<WebSocketSession, String> sessions = new ConcurrentHashMap<>();
+
     private final ChatService chatService;
     private final ObjectMapper mapper = JsonMapper.builder()
             .addModule(new ParameterNamesModule())
@@ -38,23 +42,29 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        Optional<User> user = chatService.extractUserFromWebSocketUri(session);
-        if (user.isEmpty()) throw new Exception();
+        User user = chatService.extractUserFromWebSocketUri(session)
+                .orElseThrow(Exception::new);
 
-        map.put(user.get().getUuid(), session);
+        String userId = user.getUuid();
+
+        userIds.put(userId, session);
+        sessions.put(session, userId);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        String userId = sessions.remove(session);
+        this.userIds.remove(userId);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        Optional<User> user = chatService.extractUserFromWebSocketUri(session);
-        if (user.isEmpty()) throw new Exception();
-
         MessageRequest messageRequest = mapper.readValue(message.getPayload(), MessageRequest.class);
-        String senderId = user.get().getUuid();
+
+        String senderId = sessions.get(session);
         String receiverId = messageRequest.getReceiverId();
 
-        Optional<WebSocketSession> sender = Optional.ofNullable(map.get(user.get().getUuid()));
-        Optional<WebSocketSession> receiver = Optional.ofNullable(map.get(messageRequest.getReceiverId()));
+        WebSocketSession receiver = userIds.get(receiverId);
 
         //send typing notification
         if (messageRequest.getContent() == null) {
@@ -65,40 +75,18 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
                     .content(null)
                     .build();
             String json = mapper.writeValueAsString(messageDTO);
-            receiver.ifPresent(u -> {
-                try {
-                    u.sendMessage(new TextMessage(json));
-                } catch (IOException e) {
-                }
-            });
+            if (receiver != null)
+                receiver.sendMessage(new TextMessage(json));
 
             return;
         }
 
         //send actual message
-        MessageDTO messageDTO = messageService.sendMessage(user.get(), messageRequest);
+        MessageDTO messageDTO = messageService.sendMessage(senderId, messageRequest);
         String json = mapper.writeValueAsString(messageDTO);
 
-        sender.ifPresent(u -> {
-            try {
-                u.sendMessage(new TextMessage(json));
-            } catch (IOException e) {
-            }
-        });
-        receiver.ifPresent(u -> {
-            try {
-                u.sendMessage(new TextMessage(json));
-            } catch (IOException e) {
-            }
-        });
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        Optional<User> user = chatService.extractUserFromWebSocketUri(session);
-        if (user.isEmpty()) throw new Exception();
-
-
-        map.remove(user.get().getUuid());
+        session.sendMessage(new TextMessage(json));
+        if (receiver != null)
+            receiver.sendMessage(new TextMessage(json));
     }
 }
